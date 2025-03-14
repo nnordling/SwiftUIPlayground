9d087ae431f40280c9c76b2676e375bc5123dbda
Niclas Nordling <niclasnordling@gmail.com>
2025-03-14 21:43:34 +0100

    Initial Commit

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Niclas Nordling on 2025-03-14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
